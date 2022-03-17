8ec3eb0989bf58a8c5640a65f1e40339c12fd7d5
som-syom <dhyeon@student.42seoul.kr>
2022-03-17 13:22:30 +0900

    ADD - 덱 문제 메모리초과

---
+++ 2346.js
@@ -0,0 +1,20 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const N = +input[0];
+const nums = input[1].split(" ").map((el, idx) => [+el, idx + 1]);
+let cur = nums[0][0] > 0 ? nums[0][0] - 1 : nums[0][0] + 1;
+let res = "" + 1;
+nums.shift();
+
+while (nums.length !== 0) {
+  if (cur > 0) {
+    for (let i = 0; i < cur; i++) nums.push(nums.shift());
+  } else {
+    for (let i = 0; i < -cur; i++) nums.unshift(nums.pop());
+  }
+  cur = nums[0][0];
+  res += " " + nums[0][1];
+  nums.shift();
+}
+
+console.log(res);
