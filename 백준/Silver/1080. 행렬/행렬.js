0e9522b0f77976c297b9bc0ceb426d943c0bf906
Dasom Hyeon <60869490+som-syom@users.noreply.github.com>
2022-04-18 08:03:54 +0900

    [Silver I] Title: 행렬, Time: 180 ms, Memory: 10108 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.js
@@ -0,0 +1,61 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const [h, w] = input.shift().split(" ").map(Number);
+
+let origin = [];
+const next = [];
+for (let i = 0; i < h * 2; i++) {
+  if (i < h) origin.push(input[i].split("").map(Number));
+  else next.push(input[i].split("").map(Number));
+}
+
+// console.log(origin);
+// console.log(next);
+let cnt = 0;
+
+const flip = (y, x) => {
+  for (let i = y; i < y + 3; i++) {
+    for (let j = x; j < x + 3; j++) {
+      origin[i][j] = 1 - origin[i][j];
+    }
+  }
+};
+
+const check = (h, w) => {
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (origin[i][j] !== next[i][j]) {
+        return -1;
+      }
+    }
+  }
+  return 0;
+};
+
+if (h < 3 || w < 3) {
+  console.log(check(h, w));
+} else {
+  // 바꾸기
+  const solution = (h, w) => {
+    for (let i = 0; i < h - 2; i++) {
+      for (let j = 0; j < w - 2; j++) {
+        if (origin[i][j] !== next[i][j]) {
+          flip(i, j);
+          cnt++;
+        }
+      }
+    }
+
+    // 검증
+    for (let i = 0; i < h; i++) {
+      for (let j = 0; j < w; j++) {
+        if (origin[i][j] !== next[i][j]) {
+          return -1;
+        }
+      }
+    }
+    return cnt;
+  };
+
+  console.log(solution(h, w));
+}
